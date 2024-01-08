97549cfa1724d3814439fc540c056e1873ad4519
ChakravarthiDinavahi <chakravarthi.dinavahi@gmail.com>
2024-01-08 22:04:38 +0530

    initial commit

---
+++ lib/whatsapp/cloud/api.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "api/version"
+
+module Whatsapp
+  module Cloud
+    module Api
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "whatsapp/cloud/api"
+
+require "minitest/autorun"
